f4f0a6e6a65a807be2c8475f5907d6326abe4396
gordonlzy <61217117+gordonlzy@users.noreply.github.com>
2020-03-08 11:22:09 +0800

    Update Knight.java
    added comments

--- src/Knight.java
+++ src/Knight.java
@@ -3,6 +3,7 @@ public class Knight extends Piece {
         super(white);
     }
 
+    // check if knight can move from start to end
     public boolean canMove(Board board, Spot start, Spot end) {
         boolean withinRange = ((end.getFile() >=0 && end.getFile() < 8) && (end.getRow() >= 0 && end.getRow() < 8));
         boolean occupied = (end.getPiece() != null);
@@ -10,26 +11,34 @@ public class Knight extends Piece {
         int rowChange = Math.abs(end.getRow() - start.getRow());
         boolean movingLikeAKnight = (fileChange * rowChange == 2);
 
+        // if end is in board
         if (withinRange) {
+            // if end is not occupied
             if (!occupied) {
                 return movingLikeAKnight;
             }
+            // end is occupied
             else {
                 boolean sameColour = (end.getPiece().isWhite() == this.isWhite());
+                // if occupied by opponent
                 if (!sameColour)
                     return movingLikeAKnight;
+                // if occupied by player
                 else
                     return false;
             }
         }
+        // end is not in board
         else
             return false;
     }
 
+    // return name, WN - white knight, BN - black knight
     public String getName() {
         return isWhite() ? "WN" : "BN";
     }
 
+    // check if piece is knight
     @Override
     public boolean isKnight() {
         return true;
