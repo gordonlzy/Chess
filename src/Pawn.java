8df326db24bccda5dc0bd2f9c93ff36f5fc019d6
gordonlzy <61217117+gordonlzy@users.noreply.github.com>
2020-03-08 11:27:33 +0800

    Update Pawn.java
    added comments, and slight change to canPromote()

--- src/Pawn.java
+++ src/Pawn.java
@@ -4,14 +4,18 @@ public class Pawn extends Piece {
         super(white);
     }
 
+    // check if pawn can move
     public boolean canMove(Board board, Spot start, Spot end) {
         boolean withinRange = ((end.getFile() >=0 && end.getFile() < 8) && (end.getRow() >= 0 && end.getRow() < 8));
         boolean occupied = (end.getPiece() != null);
 
+        // if end is in board
         if (withinRange) {
             int fileChange = Math.abs(end.getFile() - start.getFile());
             int rowChange = (end.getRow() - start.getRow());
 
+            // two types of movements, if white, pawn can move upwards one or two spots in the first move
+            // black pawn can move downwards one or two spots in the first move
             boolean whiteForwardOne = ((rowChange == 1) && (fileChange == 0) && start.getPiece().isWhite());
             boolean whiteForwardTwo = ((rowChange == 2) && (fileChange == 0) && start.getPiece().isWhite());
             boolean blackForwardOne = ((rowChange == -1) && (fileChange == 0) && !(start.getPiece().isWhite()));
@@ -52,19 +56,24 @@ public class Pawn extends Piece {
             return false;
     }
 
+    // return name, WP - white pawn, BP - black pawn
     public String getName() {
         return (isWhite() ? "WP" : "BP");
     }
 
+    // check if piece is pawn
     @Override
     public boolean isPawn() {
         return true;
     }
 
+    // check if pawn can be promoted
     @Override
     public boolean canPromote(Spot end) {
-        if (end.getRow() == 7)
+        // if piece is white and on the uppermost row or piece is black and on the most bottom row, then can promote
+        if ((isWhite() == true && end.getRow() == 7) || (isWhite() == false && end.getRow() == 0))
             return true;
+        // else cannot promote
         else
             return false;
     }
